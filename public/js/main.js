370170b8a9e1c98527e99fce09cef827f8c84511
Behzad <92151678+abdOfHossein@users.noreply.github.com>
2022-12-30 20:38:10 +0330

    p2

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function (e) {
+  setTimeout(() => {
+    let successMsg = $("#successMsg");
+    let field1Err = $("#field1Err");
+    let field2Err = $("#field2Err");
+    let field3Err = $("#field3Err");
+    let field4Err = $("#field4Err");
+    console.log(successMsg, field1Err, field2Err, field3Err, field4Err);
+    if (
+      successMsg.html() ||
+      field1Err.html() ||
+      field2Err.html() ||
+      field3Err.html() ||
+      field4Err.html()
+    ) {
+      successMsg.html(null);
+      field1Err.html(null);
+      field2Err.html(null);
+      field3Err.html(null);
+      field4Err.html(null);
+    }
+  }, 2000);
+});
--- routes/web1.js
+++ routes/web1.js
@@ -1,9 +1,49 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('index',{title:"Hi Man"})
+router.get("/", (req, res) => {
+  res.render("index", {
+    field1Err: null,
+    field2Err: null,
+    field3Err: null,
+    field4Err: null,
+    successMsg: null,
+  });
 });
+
+router.post(
+  "/",
+  body("field1").isNumeric().withMessage("field1 must be Number"),
+  body("field2").isAlpha().withMessage("field2 must be Character"),
+  body("field3").isAlpha().withMessage("field3 must be Character"),
+  body("field4").isNumeric().withMessage("field4 must be Number"),
+  (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+      let field1Err, field2Err, field3Err, field4Err;
+      for (const err of errors.errors) {
+        if (err.param === "field2") field2Err = err.msg;
+      }
+      return res.status(400).render("index", {
+        field1Err: null,
+        field2Err,
+        field3Err: null,
+        field4Err: null,
+        successMsg: null,
+      });
+    }
+    console.log("hereeeeeeeeeeeeeeeeeee");
+    return res.render("index", {
+      field1Err: null,
+      field2Err: null,
+      field3Err: null,
+      field4Err: null,
+      successMsg: "Data saved in Database Successfully",
+    });
+  }
+);
 
 module.exports = router;
